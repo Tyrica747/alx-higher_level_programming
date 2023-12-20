380f8878a76d44f7d3658758b0c19bf460fadae5
Tyrica747 <Tyrica747@gmail.com>
2023-12-20 07:49:25 -0800

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Square module - assigns size of the square and checks for type and value"""
+
+
+class Square:
+    """defines a square with private instance attribute size """
+    def __init__(self, size=0):
+        """assigns size of the square and checks for type and value"""
+
+        if not isinstance(size, init):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise TypeError("size must be >= 0")
+        self.__size = int(size)
+
+        def area(self):
+            """public instance method returns current sqr area"""
+            return self.__size ** 2
